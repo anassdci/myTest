767c813c0cafc5d29bacc0defb684325dfaddeb3
anassdci <anassnebbari.hotmail.fr>
2019-04-25 16:30:31 +0200

    Updates

---
+++ precache-manifest.2d7f51d928d12d1476888b5c0d76a2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e124dec6c184d3300f11d62b60a4bc5",
+    "url": "/anassdci/myTest/static/media/texture-min.1e124dec.jpg"
+  },
+  {
+    "revision": "ce1901dd31099a0d88ad653d509369a7",
+    "url": "/anassdci/myTest/static/media/grains.ce1901dd.png"
+  },
+  {
+    "revision": "5e4c878d31a4be1ca09d",
+    "url": "/anassdci/myTest/static/js/runtime~main.08a07f83.js"
+  },
+  {
+    "revision": "5190a98a561115c01ea1",
+    "url": "/anassdci/myTest/static/js/main.e167b795.chunk.js"
+  },
+  {
+    "revision": "1ba87a830fcc81aad86b",
+    "url": "/anassdci/myTest/static/js/2.c12a6fb2.chunk.js"
+  },
+  {
+    "revision": "5190a98a561115c01ea1",
+    "url": "/anassdci/myTest/static/css/main.dbe2fab1.chunk.css"
+  },
+  {
+    "revision": "9ba6ccc40edf0d5d60510a6072688c97",
+    "url": "/anassdci/myTest/index.html"
+  }
+];
